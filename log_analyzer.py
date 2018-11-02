77cc006d1f0afb087b50e24b2349faeb10e6d893
pzrk00 <44637887+pzrk00@users.noreply.github.com>
2018-11-02 14:58:22 +0300

    iteration 3

--- log_analyzer.py
+++ log_analyzer.py
@@ -6,7 +6,6 @@ import json
 import logging
 import os
 import re
-import sys
 from datetime import datetime
 from pathlib import Path
 from string import Template
@@ -32,34 +31,6 @@ def get_config_filename():
     return args.config
 
 
-def load_yaml(filename):
-    """
-    load .yaml config file
-    :param filename:
-    :return: config dict
-    """
-    try:
-        with open(filename, 'r', encoding='utf-8') as config_file:
-            config_from_file = yaml.load(config_file)
-            return config_from_file
-    except Exception as e:
-        logging.exception(e)
-    return None
-
-
-def check_int(v):
-    """
-    check parameter - is int
-    :param v:
-    :return: int if correct
-    """
-    try:
-        return int(v)
-    except ValueError as e:
-        logging.exception(e)
-        return None
-
-
 def merge_config(internal_config, config_from_file):
     """
     merge internal & external config files
@@ -71,22 +42,9 @@ def merge_config(internal_config, config_from_file):
     for k in internal_config.keys():
         if k in config_from_file.keys():
             if k == 'REPORT_SIZE':
-                report_size = check_int(config_from_file[k])
-                if report_size is None:
-                    return None
-                if report_size < 1:
-                    logging.error(f'REPORT_SIZE parameter must be greater 1, config value is: {report_size}')
-                    return None
-                result[k] = report_size
+                result[k] = int(config_from_file[k])
             elif k == 'ERROR_PERCENT':
-                err_percent = check_float(config_from_file[k])
-                if err_percent is None:
-                    return None
-                if err_percent >= 100 or err_percent < 0:
-                    logging.error(f'ERROR_PERCENT parameter must be greater 0 and less 100, '
-                                  f'config value is: {err_percent}')
-                    return None
-                result[k] = err_percent
+                result[k] = float(config_from_file[k])
             else:
                 result[k] = config_from_file[k]
         else:
@@ -94,28 +52,23 @@ def merge_config(internal_config, config_from_file):
     return result
 
 
-def load_config(internal_config):
+def load_config(internal_config, cfg_filename):
     """
     load external config file(yaml) if exist
+    :param internal_config:
+    :param cfg_filename: external config file name
     :return: config dict
     """
-    cfg_filename = get_config_filename()
     if cfg_filename:
-        cfg = load_yaml(cfg_filename)
-        if cfg:
-            return merge_config(internal_config, cfg)
-        else:
+        try:
+            with open(cfg_filename, 'r', encoding='utf-8') as config_file:
+                cfg = yaml.safe_load(config_file)
+        except Exception as e:
+            logging.exception(e)
             return None
+        return merge_config(internal_config, cfg)
     else:
-        cfg_filename = os.path.splitext(os.path.basename(__file__))[0] + '.yaml'
-        if Path(cfg_filename).is_file():
-            cfg = load_yaml(cfg_filename)
-            if cfg:
-                return merge_config(internal_config, cfg)
-            else:
-                return None
-        else:
-            return config
+        return internal_config
 
 
 def median(lst):
@@ -177,7 +130,8 @@ def parse_log(fn, gz=False):
     count = 0
     error_count = 0
     try:
-        with open(fn, 'r', encoding='utf-8') if not gz else gzip.open(fn, 'rt', encoding='utf-8') as log_file:
+        opener = gzip.open if gz else open
+        with opener(fn, 'rt', encoding='utf-8') as log_file:
             for line in log_file:
                 count += 1
                 url, req_time = parse_line(line)
@@ -211,35 +165,40 @@ def calc_stat(data, all_count, all_time, report_size):
         return None
     i = 0
     d = list()
-    # all_count = len(data)
-
     max_size = report_size if len(data) > report_size else len(data)
-    for r in data:
-        count = len(data[r])
-        count_perc = round(count * 100 / all_count, 3)
-        time_sum = round(sum(data[r]), 3)
-        time_max = round(max(data[r]), 3)
-        time_avg = round(time_sum / count, 3)
-        time_perc = round(time_sum * 100 / all_time, 3)
-        time_med = round(median(data[r]), 3)
+    try:
+        for r in data:
+            count = len(data[r])
+            count_perc = round(count * 100 / all_count, 3)
+            time_sum = round(sum(data[r]), 3)
+            time_max = round(max(data[r]), 3)
+            time_avg = round(time_sum / count, 3)
+            time_perc = round(time_sum * 100 / all_time, 3)
+            time_med = round(median(data[r]), 3)
 
-        d.append({'url': r, 'count': count, 'count_perc': count_perc, 'time_avg': time_avg,
-                  'time_max': time_max, 'time_med': time_med, 'time_perc': time_perc, 'time_sum': time_sum})
-        if i == max_size:
-            break
-        else:
-            i += 1
-    logging.info('statistics calculated')
-    return json.dumps(d)
+            d.append({'url': r, 'count': count, 'count_perc': count_perc, 'time_avg': time_avg,
+                      'time_max': time_max, 'time_med': time_med, 'time_perc': time_perc, 'time_sum': time_sum})
+            if i == max_size:
+                break
+            else:
+                i += 1
+        logging.info('statistics calculated')
+        return json.dumps(d)
+    except Exception as e:
+        logging.error(e)
+        return None
 
 
-def create_report(data, rep_file):
+def create_report(data, rep_file, rep_path):
     """
     fill template and save it to report file
     :param data: json data
     :param rep_file: report file name
+    :param rep_path: report path
     :return: none
     """
+    rep_file = os.path.join(rep_path, rep_file)
+    logging.info(rep_file)
     if data and len(data) > 0:
         try:
             with open('report.html', 'r') as f:
@@ -269,18 +228,14 @@ def parse_filename_extension(extension):
         return None
 
 
-def find_log_files(log_folder, rep_folder):
+def find_log_files(log_folder):
     """
     make dict: {date, log_file_name, report_file_name, is_gz: Boolean}
     :param log_folder: logs files folder
-    :param rep_folder: reports files folder
     :return: dict: {date, log_file_name, report_file_name, is_gz: Boolean}
     """
     if not Path(log_folder).is_dir():
         logging.error(f'"{log_folder}" is not a folder')
-        return None
-    if not Path(rep_folder).is_dir():
-        logging.error(f'"{rep_folder}" is not a folder')
         return None
 
     result = list()
@@ -289,45 +244,46 @@ def find_log_files(log_folder, rep_folder):
         if len(fname) > 1 and fname[0] == 'nginx-access-ui':
             date_parts = parse_filename_extension(fname[1])
             if date_parts:
-                rep_file_name = 'report-' + '.'.join(date_parts) + '.html'
                 row = dict()
+                row['rep_file'] = 'report-' + '.'.join(date_parts) + '.html'
                 row['date'] = ''.join(date_parts)
                 row['log_file'] = log_folder + filename
-                row['rep_file'] = rep_folder + rep_file_name
                 row['gz'] = True if len(fname) == 3 and fname[2] == 'gz' else False
                 result.append(row)
     result = sorted(result, key=lambda k: k['date'], reverse=True)
-    if len(result) > 0 and not Path(result[0]['rep_file']).is_file():
-        return result[0]
-    else:
-        return dict()
+    return result[0] if len(result) > 0 else None
 
 
 def main(internal_config):
-    cfg = load_config(internal_config)
-    if not cfg:
-        return
-    logging.basicConfig(format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S',
-                        level=logging.INFO, filename=cfg['LOG_FILE'])
-    logging.info(cfg)
+    try:
+        cfg = load_config(internal_config, get_config_filename())
+        if not cfg:
+            return
+        logging.basicConfig(format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S',
+                            level=logging.INFO, filename=cfg['LOG_FILE'])
 
-    log_folder = cfg['LOG_DIR'] + '/'
-    log_file = find_log_files(log_folder, cfg['REPORT_DIR'] + '/')
+        log_folder = os.path.join(cfg['LOG_DIR'], '')
+        rep_folder = os.path.join(cfg['REPORT_DIR'], '')
+        log_file = find_log_files(log_folder)
 
-    if log_file is not None:
-        if len(log_file) > 0:
-            data, all_time, count, error_count = parse_log(log_file['log_file'], log_file['gz'])
-            error_percent = (error_count / count) * 100
-            logging.info(f'row count is: {count}, errors count is: {error_count}, '
-                         f'errors percent is: {error_percent:.2f}')
-            if error_percent > cfg['ERROR_PERCENT']:
-                logging.info(f'error percent({error_percent:.2f}) '
-                             f'exceeded specified value({cfg["ERROR_PERCENT"]:.2f}), cannot create report')
-                return
-            json_data = calc_stat(data, all_time, cfg['REPORT_SIZE'])
-            create_report(json_data, log_file['rep_file'])
-        else:
+        if log_file is None:
             logging.info(f'the folder: {log_folder} has no files to process')
+            return
+
+        data, all_time, count, error_count = parse_log(log_file['log_file'], log_file['gz'])
+        if data is None:
+            return
+        error_percent = (error_count / count) * 100
+        logging.info(f'row count is: {count}, errors count is: {error_count}, '
+                     f'errors percent is: {error_percent:.2f}')
+        if error_percent > cfg['ERROR_PERCENT']:
+            logging.info(f'error percent({error_percent:.2f}) '
+                         f'exceeded specified value({cfg["ERROR_PERCENT"]:.2f}), cannot create report')
+            return
+        json_data = calc_stat(data, count, all_time, cfg['REPORT_SIZE'])
+        create_report(json_data, log_file['rep_file'], rep_folder)
+    except:
+        logging.exception('unexpected error')
 
 
 if __name__ == "__main__":
